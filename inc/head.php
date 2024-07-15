8ccf4f2dee1570025ae8cb8e681b710c616a6217
AlejandroIdk <elvatoldk0072@gmail.com>
2024-07-15 13:55:46 -0500

    logout

--- inc/head.php
+++ inc/head.php
@@ -8,4 +8,7 @@
 <link rel="shortcut icon" href="./assets/img/logo.png" class="rounded-circle" width="10" height="10" type="image/x-icon">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.css">
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.css">
    
