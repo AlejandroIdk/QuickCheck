26d6f665541b51ee0d21ed69440564b82d2f2fc7
AlejandroIdk <elvatoldk0072@gmail.com>
2024-07-31 12:11:19 -0500

    footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,40 +1,43 @@
 </section>
-	</main>
+</main>
 
+<!-- Vendor JS Files -->
+<script src="./assets/vendor/apexcharts/apexcharts.min.js"></script>
+<script src="./assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="./assets/vendor/simple-datatables/simple-datatables.js"></script>
 
-	<!--=============================================
-	=            Include JavaScript files           =
-	==============================================-->
-	<!-- jQuery V3.4.1 -->
-	<script src="./assets/dashboard/js/jquery-3.4.1.min.js" ></script>
+<!-- jQuery V3.4.1 -->
+<script src="./assets/dashboard/js/jquery-3.4.1.min.js"></script>
 
-	<!-- popper -->
-	<script src="./assets/dashboard/js/popper.min.js" ></script>
+<!-- Popper -->
+<script src="./assets/dashboard/js/popper.min.js"></script>
 
-	<!-- Bootstrap V4.3 -->
-	<script src="./assets/dashboard/js/bootstrap.min.js" ></script>
+<!-- Bootstrap V4.3 -->
+<script src="./assets/dashboard/js/bootstrap.min.js"></script>
 
-	<!-- jQuery Custom Content Scroller V3.1.5 -->
-	<script src="./assets/dashboard/js/jquery.mCustomScrollbar.concat.min.js" ></script>
+<!-- jQuery Custom Content Scroller V3.1.5 -->
+<script src="./assets/dashboard/js/jquery.mCustomScrollbar.concat.min.js"></script>
 
-	<script src="./assets/dashboard/js/main.js" ></script>
+<!-- Template Main JS File -->
+<script src="./assets/js/main.js"></script>
 
-  <!-- ======= Footer ======= -->
-  <footer id="footer" class="footer">
-    <div class="copyright">
-      &copy; Copyright <strong><span>QuickCheck</span></strong>. Viva el fulboooooooooooo
-    </div>
-  </footer>
+<!-- ======= Footer ======= -->
+<footer id="footer" class="footer">
+  <div class="copyright">
+    &copy; Copyright <strong><span>QuickCheck</span></strong>. Viva el fulboooooooooooo
+  </div>
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <p class="has-text-centered mb-4 mt-3">
+    <a data-mdb-ripple-init class="btn text-white btn-floating m-1" style="background-color: #3b5998;" href="https://facebook.com/elvato.idk.07" role="button"><i class="fab fa-facebook-f"></i></a>
+    <a data-mdb-ripple-init class="btn text-white btn-floating m-1" style="background-color: #55acee;" href="https://twitter.com" role="button"><i class="fab fa-twitter"></i></a>
+    <a data-mdb-ripple-init class="btn text-white btn-floating m-1" style="background-color: #dd4b39;" href="https://gmail.com" role="button"><i class="fab fa-google"></i></a>
+    <a data-mdb-ripple-init class="btn text-white btn-floating m-1" style="background-color: #ac2bac;" href="https://instagram.com" role="button"><i class="fab fa-instagram"></i></a>
+    <a data-mdb-ripple-init class="btn text-white btn-floating m-1" style="background-color: #0082ca;" href="https://linkedin.com" role="button"><i class="fab fa-linkedin-in"></i></a>
+    <a data-mdb-ripple-init class="btn text-white btn-floating m-1" style="background-color: #333333;" href="https://github.com/AlejandroIdk" role="button"><i class="fab fa-github"></i></a>
+  </p>
+</footer>
 
-  <!-- Vendor JS Files -->
-  <script src="./assets/vendor/apexcharts/apexcharts.min.js"></script>
-  <script src="./assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="./assets/vendor/simple-datatables/simple-datatables.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="./assets/js/main.js"></script>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
 </body>
 
